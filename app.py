a8752cc5f726a708e3c446c5929ace0562106929
wombat14142 <83711089+wombat14142@users.noreply.github.com>
2024-10-16 10:41:54 +0200

    setup dashboard fe

--- app.py
+++ app.py
@@ -1,41 +1,27 @@
-import requests
+# app.py
+import dash
+from dash import dcc, html
 
-def get_weather_data(lat, lon, api_key):
-    # API-Endpunkt und Parameter
-    #base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    base_url = "http://api.openweathermap.org/data/2.5/forecast?"
-    #complete_url = f"{base_url}q={city_name}&appid={api_key}&units=metric"
-    complete_url = f"{base_url}lat={lat}&lon={lon}&appid={api_key}"
+app = dash.Dash(__name__)
 
-    # Anfrage an die OpenWeatherMap API senden
-    response = requests.get(complete_url)
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+    html.Div(children='Dash: A web application framework for Python.'),
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'NYC'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
 
-    # Antwort im JSON-Format
-    data = response.json()
+server = app.server
 
-    # Überprüfen, ob die Anfrage erfolgreich war
-    if data["cod"] != "404":
-        # Daten extrahieren
-        main = data["main"]
-        weather = data["weather"][0]
-        wind = data["wind"]
-
-        # Wetterinformationen anzeigen
-        print(f"Stadt: {city_name}")
-        print(f"Temperatur: {main['temp']}°C")
-        print(f"Gefühlte Temperatur: {main['feels_like']}°C")
-        print(f"Luftfeuchtigkeit: {main['humidity']}%")
-        print(f"Wetterbeschreibung: {weather['description']}")
-        print(f"Windgeschwindigkeit: {wind['speed']} m/s")
-    else:
-        print("Stadt nicht gefunden.")
-
-# Deine API-Schlüssel und die Stadt
-api_key = "8eca9d1bfe0e3f44a51b72c6e4ade2d2"
-#city_name = input("Gib den Namen der Stadt ein: ")
-# example london
-lat = 51.5073219
-lon = -0.1276474
-
-# Wetterdaten abrufen
-get_weather_data(lat, lon, api_key)
+if __name__ == '__main__':
+    app.run_server(debug=True)
